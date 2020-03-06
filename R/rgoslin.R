b66ca2a5d68458af211e1a3e216746bec31454b1
Nils Hoffmann <nils.hoffmann@isas.de>
2020-03-06 18:32:02 +0100

    Integrated cppgoslin.

---
+++ R/rgoslin.R
@@ -0,0 +1,20 @@
+#' @useDynLib rgoslin, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+#' @importFrom Rcpp evalCpp
+NULL
+
+#' Check the provided lipid name against the built-in grammars.
+#' Will return FALSE if none of the parsers was able to parse the provided name successfully.
+#' @param lipidName the lipid name to check.
+#' @export
+isValidLipidName <- function(lipidName) {
+  return(rgoslin::rcpp_is_valid_lipid_name(lipidName))
+}
+
+#' Parse the provided lipid name and return structural information.
+#' Will return an empty list if none of the parsers was able to parse the provided name successfully.
+#' @param lipidName the lipid name to check.
+#' @export
+parseLipidName <- function(lipidName) {
+  return(rgoslin::rcpp_parse_lipid_name(lipidName))
+}
---
+++ src/rgoslin.cpp
@@ -0,0 +1,51 @@
+#define STRICT_R_HEADERS
+#include <Rcpp.h>
+#include <cppgoslin/cppgoslin/cppgoslin.h>
+
+using namespace Rcpp;
+
+// via the exports attribute we tell Rcpp to make this function
+// available from R
+//
+// [[Rcpp::plugins("cpp11")]]
+// [[Rcpp::export]]
+bool rcpp_is_valid_lipid_name(std::string lipid_name) {
+    /* create instance of lipid parser containing several grammars */
+    LipidParser lipid_parser;
+
+    /* parsing lipid name into a lipid container data structure */
+    LipidAdduct* lipidAdduct = lipid_parser.parse(lipid_name);
+
+    /* checking if parsing was successful, otherwise lipid reference remains NULL */
+    bool isValidLipidName = false;
+    if (lipidAdduct != NULL){
+        isValidLipidName = true;
+    }
+
+    return isValidLipidName;
+}
+
+// via the exports attribute we tell Rcpp to make this function
+// available from R
+//
+// [[Rcpp::plugins("cpp11")]]
+// [[Rcpp::export]]
+RcppExport SEXP rcpp_parse_lipid_name(std::string lipid_name) {
+    /* create instance of lipid parser containing several grammars */
+    LipidParser lipid_parser;
+    
+    /* parsing lipid name into a lipid container data structure */
+    LipidAdduct* lipidAdduct = lipid_parser.parse(lipid_name);
+    std::map<std::string, std::string> lipidDetails;
+    if (lipidAdduct != NULL){
+        LipidSpeciesInfo info = lipidAdduct->lipid->info;
+        std::string nativeLevelName = lipidAdduct->get_lipid_string(info.level);
+        std::string category = lipidAdduct->get_lipid_string(CATEGORY);
+        std::string species = lipidAdduct->get_lipid_string(SPECIES);
+        lipidDetails["originalName"] = lipid_name;
+        lipidDetails["nativeLevelName"] = nativeLevelName;
+        lipidDetails["category"] = category;
+        lipidDetails["species"] = species;
+    }
+    return Rcpp::wrap(lipidDetails);
+}
