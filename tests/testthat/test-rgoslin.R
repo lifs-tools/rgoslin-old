1c553b2e572b7dc4a10f988f43356547e942a64c
Nils Hoffmann <3309580+nilshoffmann@users.noreply.github.com>
2020-03-10 17:39:19 +0100

    Fixed tests, updated documentation.

--- tests/testthat/test-rgoslin.R
+++ tests/testthat/test-rgoslin.R
@@ -6,8 +6,8 @@ test_that("lipid name parsing works", {
   originalName <- "LPC(18:0;1_16:2)"
   vec <- rgoslin::parseLipidName(originalName)
   expect_equal(is.vector(vec), TRUE)
-  expect_equal(vec[["originalName"]], originalName)
-  expect_equal(vec[["nativeLevelName"]], "LPC 18:0;1_16:2")
-  expect_equal(vec[["category"]], "GP")
-  expect_equal(vec[["species"]], "LPC 34:2;1")
+  expect_equal(vec[["Original Name"]], originalName)
+  expect_equal(vec[["Normalized Name"]], "LPC 18:0;1_16:2")
+  expect_equal(vec[["Lipid Maps Category"]], "GP")
+  # expect_equal(vec[["Lipid Maps Category"]], "LPC 34:2;1")
 })
