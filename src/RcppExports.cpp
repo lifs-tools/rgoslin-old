a5fbc24f47ecead84deb814d0a38b02269b8cff5
Nils Hoffmann <nils.hoffmann@isas.de>
2020-04-16 14:23:01 +0200

    Updated cpp-r binding. Added method to parse lipid names with a specific grammar.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -16,6 +16,18 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// rcpp_parse_lipid_name_with_grammar
+SEXP rcpp_parse_lipid_name_with_grammar(std::string lipid_name, std::string target_grammar);
+RcppExport SEXP _rgoslin_rcpp_parse_lipid_name_with_grammar(SEXP lipid_nameSEXP, SEXP target_grammarSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< std::string >::type lipid_name(lipid_nameSEXP);
+    Rcpp::traits::input_parameter< std::string >::type target_grammar(target_grammarSEXP);
+    rcpp_result_gen = Rcpp::wrap(rcpp_parse_lipid_name_with_grammar(lipid_name, target_grammar));
+    return rcpp_result_gen;
+END_RCPP
+}
 // rcpp_parse_lipid_name
 SEXP rcpp_parse_lipid_name(std::string lipid_name);
 RcppExport SEXP _rgoslin_rcpp_parse_lipid_name(SEXP lipid_nameSEXP) {
@@ -30,6 +42,7 @@ END_RCPP
 
 static const R_CallMethodDef CallEntries[] = {
     {"_rgoslin_rcpp_is_valid_lipid_name", (DL_FUNC) &_rgoslin_rcpp_is_valid_lipid_name, 1},
+    {"_rgoslin_rcpp_parse_lipid_name_with_grammar", (DL_FUNC) &_rgoslin_rcpp_parse_lipid_name_with_grammar, 2},
     {"_rgoslin_rcpp_parse_lipid_name", (DL_FUNC) &_rgoslin_rcpp_parse_lipid_name, 1},
     {NULL, NULL, 0}
 };
--- tests/testthat/test-rgoslin.R
+++ tests/testthat/test-rgoslin.R
@@ -9,7 +9,22 @@ test_that("lipid name parsing works", {
   expect_equal(vec[["Original Name"]], originalName)
   expect_equal(vec[["Normalized Name"]], "LPC 34:2;1")
   expect_equal(vec[["Lipid Maps Category"]], "GP")
-  # expect_equal(vec[["Lipid Maps Category"]], "LPC 34:2;1")
+})
+
+test_that("lipid name parsing with grammar works", {
+  originalName <- "LPC(34:1)"
+  vec <- rgoslin::parseLipidNameWithGrammar(originalName, "SwissLipids")
+  expect_equal(is.vector(vec), TRUE)
+  expect_equal(vec[["Original Name"]], originalName)
+  expect_equal(vec[["Normalized Name"]], "LPC 34:1")
+  expect_equal(vec[["Lipid Maps Category"]], "GP")
+  
+  originalName <- "TG(16:1(5E)/18:0/20:2(3Z,6Z))"
+  vec <- rgoslin::parseLipidNameWithGrammar(originalName, "LipidMaps")
+  expect_equal(is.vector(vec), TRUE)
+  expect_equal(vec[["Original Name"]], originalName)
+  expect_equal(vec[["Normalized Name"]], "TAG 16:1(5E)/18:0/20:2(3Z,6Z)")
+  expect_equal(vec[["Lipid Maps Category"]], "GL")
 })
 
 test_that("multiple lipid names parsing works", {
@@ -25,4 +40,17 @@ test_that("multiple lipid names parsing works", {
   expect_equal(as.character(df[2, "Normalized Name"]), "LPC 34:2;1")
   expect_equal(as.character(df[3, "Normalized Name"]), "TAG 18:1-18:0-16:1")
   expect_equal(as.character(df[4, "Normalized Name"]), "TAG 16:1/18:0/20:2")
+})
+
+test_that("multiple lipid names parsing with grammar works", {
+  originalNames <- c("PC 32:1","LPC 34:1","TAG 18:1_18:0_16:1")
+  df <- rgoslin::parseLipidNamesWithGrammar(originalNames, "Goslin")
+  expect_equal(is.data.frame(df), TRUE)
+  expect_equal(nrow(df), 3)
+  expect_equal(as.character(df[1, "Original Name"]), originalNames[[1]])
+  expect_equal(as.character(df[2, "Original Name"]), originalNames[[2]])
+  expect_equal(as.character(df[3, "Original Name"]), originalNames[[3]])
+  expect_equal(as.character(df[1, "Normalized Name"]), "PC 32:1")
+  expect_equal(as.character(df[2, "Normalized Name"]), "LPC 34:1")
+  expect_equal(as.character(df[3, "Normalized Name"]), "TAG 18:1-18:0-16:1")
 })
