531603b6e997a65faa51b2e127d4bf66de39bbf8
Dominik Kopczynski <dominik.kopczynski@tu-dortmund.de>
2021-08-03 07:50:39 +0200

    updated build and install script

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-R -e "library('devtools')" -e "devtools::build(binary = TRUE, args = c('--preclean'))"
-R -e "install.packages(\"../rgoslin_2.0.1.1000_R_x86_64-pc-linux-gnu.tar.gz\", repos = NULL, type=\"source\")"
+R -e "library('devtools')" -e "install(\".\")"
